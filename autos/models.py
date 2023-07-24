6055b096cd2944e6e356686a8abf22b4e7eeb3a0
yunchengwong <yuncheng.wong@hotmail.com>
2023-07-24 18:09:03 +0800

    Autos Schema

--- autos/models.py
+++ autos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core import validators
 
 # Create your models here.
+
+class Auto(models.Model):
+    name = models.CharField(max_length=200, validators=[validators.MinValueValidator(2,"Make must be greater than 1 character")])
+    mileage = models.PositiveIntegerField()
+    comments = models.CharField(max_length=300)
+    make = models.ForeignKey('Make', on_delete=models.CASCADE, null=False)
+    
+    def __str__(self):
+        return self.name
+    
+class Make(models.Model):
+    name = models.CharField(max_length=200, validators=[validators.MinValueValidator(2,"Make must be greater than 1 character")], help_text="Enter a Auto manufacturer")
+    
+    def __str__(self):
+        return self.name
